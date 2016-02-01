ec5f6341b28ca21c17e7ab4f52977583b1fd7df0
Marcos J. Alvarez <garusis@gmail.com>
2016-01-31 20:22:57 -0500

    Creacion de modelos y relaciones embebidas para cursos

--- server/boot/migrate.js
+++ server/boot/migrate.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /**
  * Created by Marcos J. Alvarez on 03/01/2016.
  * @author Marcos J. Alvarez
@@ -6,80 +6,50 @@
  * @version 0.0.1
  */
 (() => {
-  const app = require("../server");
-  const _ = require("lodash");
-  const async = require("async");
+  const app = require('../server');
+  const fs = require('fs');
+  const _ = require('lodash');
+  const async = require('async');
+  const Promise = require('bluebird');
+
 
   const Role = app.models.Role;
   const RoleMapping = app.models.RoleMapping;
   const memoryDataSource = app.dataSources.memory;
 
-  memoryDataSource.automigrate(["Gender", "AccessToken", "ACL"], (err) => {
-    const Gender = memoryDataSource.models.Gender;
-
-    Gender.create([{
-      name: "Masculino",
-      short: "m"
-    }, {
-      name: "Femenino",
-      short: "f"
-    }], (err) => {
-
+  const seedModel = function (Model) {
+    const seederBasePath = './seeds';
+    var dataToSeed = require(`${seederBasePath}/${Model.definition.name}`);
+    return new Promise(function (resolve, reject) {
+      Model.create(dataToSeed, function (err, data) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      })
     });
+  };
+
+  memoryDataSource.automigrate(['Gender', 'AccessToken', 'ACL'], function (err) {
+
+    const Gender = memoryDataSource.models.Gender;
+    seedModel(Gender);
 
     const mongodbDataSource = app.dataSources.mongodb;
 
-    mongodbDataSource.automigrate(["RoleMapping", "Role", "AppUser", "Comment", "Instructor", "Publication", "PublicPublication", "Student", "Testimony"], (err)=> {
-      mongodbDataSource.autoupdate(["RoleMapping", "Role", "AppUser", "Comment", "Instructor", "Publication", "PublicPublication", "Student", "Testimony"], (err) => {
+    mongodbDataSource.automigrate(['RoleMapping', 'Role', 'AppUser', 'Comment', 'Instructor', 'Publication', 'PublicPublication', 'Student', 'Testimony'], (err)=> {
+      mongodbDataSource.autoupdate(['RoleMapping', 'Role', 'AppUser', 'Comment', 'Instructor', 'Publication', 'PublicPublication', 'Student', 'Testimony'], (err) => {
         const User = mongodbDataSource.models.AppUser;
+        const Testimony = mongodbDataSource.models.Testimony;
+        const PublicPublication = mongodbDataSource.models.PublicPublication;
+        const Publication = mongodbDataSource.models.Publication;
+        const Course = mongodbDataSource.models.Course;
 
-        Role.create([
-          {name: "admin"},
-          {name: "instructor"},
-          {name: "student"},
-          {name: "suscriptor"}
-        ], (err, roles) => {
-          roles = _.indexBy(roles, "name");
-
-          User.create([
-            {
-              "name": "Marcos J.",
-              "lastname": "Alvarez M.",
-              "username": "garusis",
-              "email": "garusis@gmail.com",
-              "emailVerified": true,
-              "password": "123456",
-              "created": "2016-01-10",
-              "lastUpdated": "2016-01-10",
-              "lastAccess": "2016-01-10",
-              "genderShort": "m"
-            },
-            {
-              "name": "Marco Lino",
-              "lastname": " Alvarez B.",
-              "username": "marlininternacional",
-              "email": "marlininternacional@hotmail.com",
-              "emailVerified": true,
-              "password": "123456",
-              "created": "2016-01-10",
-              "lastUpdated": "2016-01-10",
-              "lastAccess": "2016-01-10",
-              "genderShort": "m"
-            },
-            {
-              "name": "Yanidis Josefina",
-              "lastname": "Maestre D.",
-              "username": "yanidisjos",
-              "email": "yanidisjos@gmail.com",
-              "emailVerified": true,
-              "password": "123456",
-              "created": "2016-01-10",
-              "lastUpdated": "2016-01-10",
-              "lastAccess": "2016-01-10",
-              "genderShort": "f"
-            }
-          ], (err, users) => {
-            users = _.indexBy(users, "username");
+        Promise
+          .resolve([seedModel(Role), seedModel(User)])
+          .spread(function (roles, users) {
+            roles = _.indexBy(roles, 'name');
+            users = _.indexBy(users, 'username');
 
             roles.admin.principals.create({
               principalType: RoleMapping.USER,
@@ -99,11 +69,22 @@
             users.marlininternacional.becomeInstructor();
             users.marlininternacional.save();
 
-            User.login({username: users.garusis.username, password: "123456"}, (err, token) => {
+            User.login({username: users.garusis.username, password: '123456'}, function (err, token) {
               console.log(token);
             });
+
+            seedModel(PublicPublication)
+              .then(function () {
+                Publication.updateAll({}, {instructorId: users.marlininternacional.id}, function (err) {
+                });
+              });
+            seedModel(Course)
+              .then(function (elems) {
+                console.log(elems);
+              });
           });
-        });
+        seedModel(Testimony);
+
       });
     });
   });
