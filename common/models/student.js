8110fbca726d2be54e4143a6add94c709a360fa6
Marcos J. Alvarez <garusis@gmail.com>
2017-02-21 00:50:18 -0500

    Add public acls

--- common/models/student.js
+++ common/models/student.js
@@ -1,5 +1,14 @@
-'use strict';
+"use strict"
+import ModelBuilder from "loopback-build-model-helper"
 
-module.exports = function(Student) {
+module.exports = function(_Student) {
+  let builder = new ModelBuilder(Student, _Student)
 
+  builder.build().then(function () {
+    _Student.nestRemoting("courses")
+  })
+
+  function Student() {
+
+  }
 };
