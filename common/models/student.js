86547e0b222001555eaa5a28def15d06dc2a4fb4
Marcos J. Alvarez <garusis@gmail.com>
2017-02-21 21:47:37 -0500

    add first password field

--- common/models/student.js
+++ common/models/student.js
@@ -6,6 +6,16 @@ module.exports = function(_Student) {
 
   builder.build().then(function () {
     _Student.nestRemoting("courses")
+
+    _Student.observe('before save', function(ctx, next) {
+
+      let instance = ctx.instance || ctx.data
+      if(!ctx.isNewInstance && instance.password){
+        instance.firstPassword = false
+      }
+
+      next();
+    });
   })
 
   function Student() {
