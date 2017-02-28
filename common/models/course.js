23ba76e6e6e3f4d9669dec51da25e2f19878cc9f
Marcos J. Alvarez <garusis@gmail.com>
2017-02-27 20:00:00 -0500

    Add nest remoting modules

--- common/models/course.js
+++ common/models/course.js
@@ -1,5 +1,14 @@
 'use strict';
+import ModelBuilder from "loopback-build-model-helper"
 
-module.exports = function(Course) {
+module.exports = function (_Course) {
+  let builder = new ModelBuilder(Course, _Course)
 
+  builder.build().then(function () {
+    _Course.nestRemoting("modules")
+  })
+
+  function Course () {
+
+  }
 };
