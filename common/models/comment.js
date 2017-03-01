adb392fd991e1c5ab6fdc8ce7452bcea8bfa311e
Marcos J. Alvarez <garusis@gmail.com>
2017-03-01 09:10:58 -0500

    Add recaptcha support

--- common/models/comment.js
+++ common/models/comment.js
@@ -1,5 +1,6 @@
 "use strict";
 import ModelBuilder from "loopback-build-model-helper"
+import request from "request-promise"
 import * as commonOp from "../../server/helpers/common-operations"
 import app from "../../server/server"
 
@@ -28,12 +29,33 @@ module.exports = function (_Comment) {
   Comment.create = async function () {
     let {data, options, oldCreate} = await commonOp.normalizeCreateWithPolymorphicOwner(arguments)
 
-
-
     if (data.userId) {
       let Account = app.models[data.account_type]
       let account = await Account.findById(data.userId)
       data.publisherName = `${account.name} ${account.surname}`
+    } else {
+      let err = new Error()
+      err.statusCode = 401
+      err.message = "Invalid Captcha"
+
+      let recaptchaResponse = data['g-recaptcha-response']
+      delete data['g-recaptcha-response']
+      if(!recaptchaResponse){
+        throw err
+      }
+
+      let response = await request.post(process.env.RECAPTCHA_VERIFY_URL, {
+        json: true,
+        body: {
+          secret:process.env.RECAPTCHA_SECRET,
+          response: data['g-recaptcha-response'],
+          remoteip: options.remoteIp
+        }
+      })
+
+      if(!response.success){
+        throw err
+      }
     }
 
     let comment = await oldCreate.call(this, data, options)
