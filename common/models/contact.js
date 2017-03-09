c4154a730e4378ae7afaabccd6f69dc625f011b1
Marcos J. Alvarez <garusis@gmail.com>
2017-03-09 11:46:19 -0500

    Add client template

--- common/models/contact.js
+++ common/models/contact.js
@@ -1,5 +1,7 @@
 "use strict"
 import ModelBuilder from "loopback-build-model-helper"
+import loopback from "loopback"
+import dh from "debug-helper"
 import app from "../../server/server"
 
 module.exports = function (_Contact) {
@@ -11,14 +13,33 @@ module.exports = function (_Contact) {
   builder.build().then(function () {
     const Mailer = app.models.Mailer
 
-
     _Contact.observe('after create', async function (ctx, next) {
       if (ctx.isNewInstance) {
         let contactRequest = ctx.instance
+
+        let templateClient = loopback.template(CONTACT_TEMPLATE_CLIENT), templateAdmin = loopback.template(CONTACT_TEMPLATE_ADMIN);
+
+        let optionsClient = {
+          from: process.env.FROM_CONTACT_EMAIL,
+          to: contactRequest.to,
+          subject: 'Gracias por contactarnos.',
+          data: contactRequest
+        };
+        optionsClient.html = templateClient(optionsClient)
+
+        let optionsAdmin = {
+          from: process.env.FROM_CONTACT_EMAIL,
+          to: process.env.FROM_CONTACT_EMAIL,
+          subject: 'Nos han contactado',
+          data: contactRequest
+        };
+        optionsAdmin.html = templateAdmin(optionsAdmin)
+
+        Mailer.send(optionsClient, (err) => {if (err) dh.debug.error(err)})
+        Mailer.send(optionsAdmin, (err) => {if (err) dh.debug.error(err)})
       }
       next()
     })
-
 
   })
 
